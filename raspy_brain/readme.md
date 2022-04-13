37a5a29a6be969113feb1f6fee0a2d8842050b3e
robotique.enac <robotique.enac@gmail.com>
2022-04-13 20:20:27 +0200

    sending remote improvement

---
+++ raspy_brain/readme.md
@@ -0,0 +1,5 @@
+Dépendances de send_remote en 'host' : (sshpass et) expect, send*
+
+Usage pour paramètres optionels à skip :
+
+./send_remote.bash "" "192.168.42.240"
--- raspy_brain/send_remote.bash
+++ raspy_brain/send_remote.bash
@@ -1,40 +1,47 @@
 #!/usr/bin/env bash
 
+
 #declare argument for the local path of a file 
 local_folder_firmware=${1:-"./.pio/build/nucleo_l476rg"}
 
 #declare argument for the ip address of the remote server
-remote_ip=${2:-192.168.42.239}
+remote_ip=${2:-'192.168.42.239'}
 
 #declare argument for password of ssh target
-remote_password=${3:sqilthezorld}
+remote_password=${3:-'sqilthezorld'}
 
 #declare argument for user account on the remote target
-remote_account=${4:ubuntu}
+remote_account=${4:-'ubuntu'}
+
+##copy the hex file the file to the remote server
+scp $local_folder_firmware/firmware.bin $remote_account@$remote_ip:/home/ubuntu/firmware.bin
 
 #connect in ssh with password
-sshpass -p $remote_password ssh pi@$remote_ip
-
-#check if stlink is installed and if not, install it with apt-get stlink-tools
-if ! [ -x "$(command -v st-flash)" ]; then
-  echo 'st-flash is not installed. Installing...'
-  sudo apt-get install -y stlink-tools
-fi
-
-#copy the hex file the file to the remote server
-scp $local_folder_firmware/firmware.bin ubuntu@$remote_ip:/home/ubuntu/firmware.bin
-
-#flash the hex file on the remote server
-ssh ubuntu@$remote_ip 'sudo st-flash write /home/ubuntu/firmware.bin' ' 0x08000000'
-
-#check if the hex file was flashed successfully 
-ssh ubuntu@$remote_ip 'sudo st-flash read 0x08000000'
-
-#and echo a message wether it's successful or not
-if [ $? -eq 0 ]; then
-  echo 'The hex file was flashed successfully'
-else
-  echo 'The hex file was not flashed successfully'
-fi
+#ssh $remote_account@$remote_ip
+#
+##expect password
+#expect "password:"
+#sleep 1
+#send "$remote_password\r"
+#
+##check if stlink is installed and if not, install it with apt-get stlink-tools
+#if ! [ -x "$(command -v st-flash)" ]; then
+#  echo 'st-flash is not installed. Installing...'
+#  sudo apt-get install -y stlink-tools
+#fi
+#
+#
+##flash the hex file on the remote server
+#ssh $remote_account@$remote_ip 'sudo st-flash write /home/$remote_account/firmware.bin' ' 0x08000000'
+#
+##check if the hex file was flashed successfully 
+#ssh $remote_account@$remote_ip 'sudo st-flash read 0x08000000'
+#
+##and echo a message wether it's successful or not
+#if [ $? -eq 0 ]; then
+#  echo 'The hex file was flashed successfully'
+#else
+#  echo 'The hex file was not flashed successfully'
+#fi
 
 
